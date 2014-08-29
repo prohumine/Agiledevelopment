a306f201e4466f63412087365ed2eb513aca5deb
Marvin Santizo <santizomarvin88@gmail.com>
2014-08-29 14:13:10 -0600

    created concerns for cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827211953) do
+ActiveRecord::Schema.define(version: 20140829195558) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
