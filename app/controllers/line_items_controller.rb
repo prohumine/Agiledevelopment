6952a432242c156ce8a7f5f85dd6a4665fe09adf
Marvin Santizo <santizomarvin88@gmail.com>
2014-09-02 11:53:38 -0600

    added delete from cart button.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,7 +33,6 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, 
-         notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
