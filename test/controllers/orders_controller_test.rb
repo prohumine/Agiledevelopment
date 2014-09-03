47876a80c680f430205438aa449898c6f4670cb3
Marvin Santizo <santizomarvin88@gmail.com>
2014-09-03 16:30:06 -0600

    changed stuff in creatjserb

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -32,7 +32,7 @@ test "requires item in cart" do
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
