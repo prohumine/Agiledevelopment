647965969a4d41be80a819ea4ee2c6c52acdfa2c
Marvin Santizo <santizomarvin88@gmail.com>
2014-09-04 16:12:57 -0600

    added bcrypt gem, and edited users test

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'rails_12factor', group: :production
 
 gem 'letter_opener', group: :development
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903200058) do
+ActiveRecord::Schema.define(version: 20140904212851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20140903200058) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
